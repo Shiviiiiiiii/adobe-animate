de307dd35944d3f1624422f4cb68e9a737b829c8
Bakowski Maximilien <96300778+Symilian8876@users.noreply.github.com>
2022-03-17 11:21:39 +0100

    suppresion boutton

--- js/feu.js
+++ js/feu.js
@@ -42,15 +42,3 @@ let feuDepart = {
         };
 
         setTimeout(feuDepart.timer, 9000);
-        feuDepart.start.onclick =  {
-            feuDepart.timer
-        }
-        feuDepart.stop.onclick = function() {
-            clearTimeout(feuDepart.t);
-        }
-        feuDepart.reset.onclick = function() {
-            feuDepart.h1.textContent = "00:00:000";
-            feuDepart.seconds = 0;
-            feuDepart.minutes = 0;
-            feuDepart.hours = 0;
-        }

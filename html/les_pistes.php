c2b8d45f3be9af5c8175b009b1c02d9ae410393f
Shiviiiiiiii <84573980+Shiviiiiiiii@users.noreply.github.com>
2022-05-04 16:52:41 +0200

    debut piste

---
+++ html/les_pistes.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+     <title>Les Pistes</title>
+      <link rel="stylesheet" href="../css/style.css">
+</head>
+
+  <body>
+
+    <?php include"entete.php"?>
+    
+    <br></br>
+    <br></br>
+    <h1>Présentation de quelques circuits:</h1>
+
+    <h2> La piste Laurent: </h2>
+      <p>
+      Ce circuit est une piste indoor en bitume d'un developpé de 200m sur laquelle est organisé des manches de Championnat de France, les course amicale et de ligue. Le record du tour est inféreur à 13s.
+      https://www.automodeleclubrumilly.fr/info-club/  
+    </p>
+</body>
+
+</html>

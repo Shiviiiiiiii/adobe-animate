0ea545d18c5d9a52fa13f9d1a64f27a298c66d36
Shiviiiiiiii <84573980+Shiviiiiiiii@users.noreply.github.com>
2022-05-04 16:15:42 +0200

    modification du  nom des images

---
+++ html/les_pistes.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+     <title>Les Pistes</title>
+      <link rel="stylesheet" href="../css/style.css">
+</head>
+
+  <body>
+
+    <?php include"entete.php"?>
+    
+    <br></br>
+    <br></br>
+    <h1>Présentation foireuse:</h1>
+</body>
+
+</html>
